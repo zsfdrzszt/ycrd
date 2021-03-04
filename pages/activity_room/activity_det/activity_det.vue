f467ea22f1820d603926f2a599d72be0f0084cd0
空中荧火 <15525142233@163.com>
2021-03-04 15:30:35 +0800

    修改

---
+++ pages/activity_room/activity_det/activity_det.vue
@@ -0,0 +1,97 @@
+<template>
+	<view>
+		<view class='act_det_title'>
+			{{acdet_title}}
+		</view>
+		<view class="det_content">
+			<u-parse :html="content" class='gkdet_center'>
+				
+			</u-parse>
+			<view class='acitvity_det_footer'>
+				<view>
+					<text>{{address}}</text>
+					<text>{{time}}</text>
+				</view>
+			</view>
+		</view>
+		<wtextsize @click="changetextsize"></wtextsize>
+	</view>
+</template>
+
+<script>
+	import wtextsize from "../../../components/w-textsize/w-textsize.vue"
+	export default{
+		components: {
+			wtextsize
+		},
+		data(){
+			return{
+				acdet_title:'人大代表第一活动小组宣讲活动',
+				content: `
+					<p>露从今夜白，月是故乡明</p>
+					<img src="https://cdn.uviewui.com/uview/swiper/2.jpg" />
+				`,
+				address:'郭家堡安宁活动室',
+				time:'2019年12月02日',
+				
+			}
+		},
+		methods: {
+				changetextsize(e) {
+					let num = parseInt(this.textsize)
+					let numtitle = parseInt(this.textSize)
+					if (e == 1) {
+						num++
+						numtitle++
+					} else if (e == 0) {
+						num--
+						numtitle--
+						if (num < 14) {
+							num = 14
+						}
+						if (numtitle < 14) {
+							numtitle = 14
+						}
+					}
+					this.textsize = num + 'px'
+					this.textSize = numtitle + 'px'
+					// console.log(num)
+				}
+			},
+	}
+</script>
+
+<style>
+	*{
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+	page{
+		padding-top:30px;
+		height: 100%;
+		background: url(@/static/text4/common_articlebg.jpg);
+		background-size:100% 100%;
+		background-repeat:no-repeat ;
+		font:14px Helvetica Neue,Helvetica,PingFang SC,Tahoma,Arial,sans-serif;
+	}
+	.act_det_title{
+		text-align: center;
+		font-size: 20px;
+		font-weight: bold;
+		margin: 20px;
+	}
+	.det_content{
+		padding: 10px;
+	}
+	.acitvity_det_footer{
+		display: flex;
+		justify-content: flex-end;
+	}
+	.acitvity_det_footer>view{
+		width: 160px;
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+	}
+</style>
---
+++ pages/activity_room/activity_list/activity_list.vue
@@ -0,0 +1,114 @@
+<template>
+	<view>
+		<view class='list_title'>
+			{{list_title}}
+		</view>
+		<view class='center_list'>
+			<view class='list_item' v-for="(item,index) in list_all" :key=item.id>
+				<navigator url='' class="list_item_a">
+					<view class='list_left'>
+						<img :src='item.img' alt="">
+					</view>
+					<view class='list_right'>
+						<view>{{item.title}}</view>
+						<view>{{item.time}}</view>
+					</view>
+				</navigator>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data(){
+			return{
+				list_title:'宣讲',
+				list_all:[{
+					id:0,
+					img:'https://qiniu.jza2c.com/uploads/20200331/FvLdb3vKoryNWFHQ0-7tOAJ9c5h1.png',
+					title:'柴永昌代表在活动室给选民进行宣讲',
+					time:'[2020年03月31日]'
+				},
+				{
+					id:1,
+					img:'https://qiniu.jza2c.com/uploads/20200331/FvLdb3vKoryNWFHQ0-7tOAJ9c5h1.png',
+					title:'柴永昌代表在活动室给选民进行宣讲',
+					time:'[2020年03月31日]'
+				},
+				{
+					id:2,
+					img:'https://qiniu.jza2c.com/uploads/20200331/FvLdb3vKoryNWFHQ0-7tOAJ9c5h1.png',
+					title:'柴永昌代表在活动室给选民进行宣讲',
+					time:'[2020年03月31日]'
+				},
+				{
+					id:3,
+					img:'https://qiniu.jza2c.com/uploads/20200331/FvLdb3vKoryNWFHQ0-7tOAJ9c5h1.png',
+					title:'柴永昌代表在活动室给选民进行宣讲',
+					time:'[2020年03月31日]'
+				}]
+			}
+		}
+	}
+</script>
+
+<style>
+	*{
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+	page{
+		padding-top:20px;
+		height: 100%;
+		background: url(@/static/text5/common_bg.jpg);
+		background-size:100% 100%;
+		background-repeat:no-repeat ;
+		font:14px Helvetica Neue,Helvetica,PingFang SC,Tahoma,Arial,sans-serif;
+	}
+	.list_title{
+		text-align: center;
+		font-size: 32px;
+		font-weight: bold;
+		letter-spacing: 3px;
+		text-shadow: #fff 1px 0 0, #fff 0 1px 0, #fff -1px 0 0, #fff 0 -1px 0;
+		color: #fc3a3b;
+		margin-bottom: 20px;
+	}
+	
+	.list_item{
+		margin: 15px 10px;
+		background-color: white;
+		box-shadow: 1px 1px 10px #ccc;
+		padding: 10px 0;
+		display: flex;
+		box-sizing: border-box;
+	}
+	.list_item_a{
+		display: flex;
+	}
+	.list_left{
+		width: 120px;
+		display: flex;
+		justify-content: center;
+		align-items: center
+	}
+	.list_left img{
+		width: 80%;
+		border-radius: 6px;
+	}
+	.list_right{
+		padding: 0 10px;
+		flex:1;
+	}
+	.list_right>view:nth-child(1){
+		border-bottom: 1px solid black;
+		position: relative;
+		line-height: 1.5em;
+		height: 3em;
+		overflow: hidden;
+		text-align: justify;
+		font-weight: bold;
+	}
+</style>
