4507d1606a7910f05e869e032e40a4cf10f74db0
武君健 <1355161386@qq.com>
2021-03-05 15:11:38 +0800

    组件

---
+++ components/arrange/arrange.vue
@@ -0,0 +1,51 @@
+<template>
+	<view class="arrange">
+		<view class="arrange_list">
+			时间
+			<view>{{list.time}}</view>
+		</view>
+		<view class="arrange_list">
+			活动名称
+			<view>{{list.title}}</view>
+		</view>
+		<view class="arrange_list">
+			{{list.space}}
+		</view>
+		<view class="arrange_list">
+			{{list.name}}
+		</view>
+		<view class="arrange_list">
+			{{list.content}}
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props:{
+			list:Object
+		},
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+.arrange{
+	width: 90%;
+	border: 1px solid #d71f07;
+	border-radius: 5px;
+	box-shadow: 1px 1px 10px #ccc;
+}
+.arrange_list{
+	border: 1px solid #d71f07;
+	height: 40px;
+	margin: 10px;
+	display: flex;
+	box-shadow: 1px 1px 10px #ccc;
+	border-radius: 4px;
+}
+</style>
---
+++ components/system/system.vue
@@ -0,0 +1,35 @@
+<template>
+	<navigator class="system" url="">
+		<view class="">{{content}}</view>
+		 <view class="system_time" v-if="time">{{time}}</view>
+	</navigator>
+</template>
+
+<script>
+	export default {
+		props:{
+			content:String,
+			time:String,
+		},
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+.system{
+	width: 90%;
+	margin: 15px 10px;
+	box-shadow: 1px 1px 10px #ccc;
+	display: block;
+	padding: 10px 5px;
+	line-height: 30px;
+}
+.system_time{
+	width: 100%;
+	text-align: right;
+}
+</style>
