f51b60aa1bc6815dd5807201cabc678121a9781b
whx9805 <2224121341@qq.com>
2021-03-06 18:40:22 +0800

    人代会期间

--- pages/meet/meet.vue
+++ pages/meet/meet.vue
@@ -9,14 +9,21 @@
 				<meets v-for="(item,index) in list" :list="item" :num="index" ></meets>
 			</scroll-view>
 		</view>
+		<view class="scroll-view" v-show="isActive == 2">
+			<scroll-view scroll-y="true" >
+				<message v-for="(item,index) in list1" :list="item" :num="index"></message>
+			</scroll-view>
+		</view>
 	</view>
 </template>
 
 <script>
 	import meets from "../../components/meets/meets.vue"
+	import message from "../../components/message/message.vue"
 	export default {
 		components: {
-			meets
+			meets,
+			message
 		},
 		data() {
 			return {
@@ -31,6 +38,17 @@
 					time: '2020-5-27',
 					int: '提出建议0件',
 					com: '全勤'
+				}],
+				list1: [{
+					txt: '关于撤村转居之后农民独生子女家庭补助金的遗留问题',
+					time: '2020-04-25',
+					con: '区人代会',
+					jie:'办理中'
+				},{
+					txt: '2020年5月27日郭家堡乡第十七届人民代表大会第七次会议',
+					time: '2020-04-25',
+					com: '区人代会',
+					jie:'办理中'
 				}]
 			}
 		},
