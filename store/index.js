f5a8ebe3c7c090aa3898c19ad336b8910cbbe5bb
whx9805 <2224121341@qq.com>
2021-03-08 10:30:36 +0800

    数据持久化

--- store/index.js
+++ store/index.js
@@ -1,21 +1,37 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import createPersistedState from 'vuex-persistedstate'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state: {
-		smartlist:[],
+		smartlist:uni.getStorageSync("_history") || []
 	},
 	getters: {
 	    doneTodos: state => {
 	      console.log("store")
 	    }
 	},
-	plugins: [
-		createPersistedState({ 
-			storage: window.sessionStorage ,
-		})
-	] 
+	mutations:{
+			SET_HISTORY_LISTS(state, history) {
+				state.historyLists = history
+			},
+			CLEAR_HISTORY(state) {
+				state.historyLists = []
+			}
+	},
+	actions: {
+			set_history({commit, state}, history){
+				let list = state.historyLists
+				list.unshift(history)
+				// 存入本地缓存
+				uni.setStorageSync('_history',list)
+				commit('SET_HISTORY_LISTS', list)
+			},
+			clear_history({commit}){
+				uni.removeStorageSync('_history')
+				commit('CLEAR_HISTORY')
+			}
+		}
+	
 })
 export default store
