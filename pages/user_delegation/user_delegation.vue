8c0137ac7c0e5154791fc91b8797a0e00c92ce2f
空中荧火 <15525142233@163.com>
2021-03-08 11:57:40 +0800

    修改

--- pages/user_delegation/user_delegation.vue
+++ pages/user_delegation/user_delegation.vue
@@ -4,7 +4,14 @@
 			<text>{{user_title_top}}</text>
 		</view>
 		<view class="user_del_content">
-			
+			<view class="user_del_item" v-for="(item,index) in user_list" :key=item.id>
+				<img :src="item.img" alt="">
+				<view class="item_content">
+					<text>{{item.name}}</text>
+					<text>{{item.job}}</text>
+				</view>
+				<a :href='item.tel'>联系</a>
+			</view>
 		</view>
 	</view>
 </template>
@@ -13,7 +20,21 @@
 	export default{
 		data(){
 			return{
-				user_title_top:'郭家堡乡联络站名录'
+				user_title_top:'郭家堡乡联络站名录',
+				user_list:[{
+					id:1,
+					name:'王绍泰',
+					tel:'tel:1363485238',
+					job:'榆次区安宁实业有限公司董事长',
+					img:'/static/user/user_tx.jpg'
+				},
+				{
+					id:2,
+					name:'王绍泰',
+					tel:'tel:1363485238',
+					job:'榆次区安宁实业有限公司董事长',
+					img:'/static/user/user_tx.jpg'
+				}]
 			}
 		}
 	}
@@ -40,10 +61,38 @@
 	}
 	.user_del_content{
 		flex: 1;
-		padding: 15px 30px;
+		padding: 15px 10px;
 		display: flex;
 		justify-content: space-between;
 		flex-wrap: wrap;
 		overflow-y: auto;
+	}
+	.user_del_item{
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		border-bottom: 1px solid #ccc;
+		padding: 10px 0;
+	}
+	.user_del_item>img{
+		width: 20%;
+	}
+	.user_del_item>a{
+		text-decoration: none;
+		text-align: center;
+		width: 10%;
+		color: #d63d6a;
+	}
+	.user_del_item .item_content{
+		flex: 1;
+		padding: 10px;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		
+	}
+	.user_del_item .item_content>text:nth-child(2){
+		color: #b0afaf;
 	}
 </style>
