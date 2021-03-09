1a4ba57f6d70e6af473a2e6df00c1a28e8859482
liuyuxia <1843353966@qq.com>
2021-03-09 11:29:59 +0800

    个人中心双联系的我的留言

--- pages/user/myMessages/myMessages.vue
+++ pages/user/myMessages/myMessages.vue
@@ -1,6 +1,7 @@
 <template>
 	<!-- 个人中心/双联系/我的留言-->
 	<view class="myMessages">
+		<wnavall></wnavall>
 		<!-- banner图 -->
 		<image class="myMessages_image" src="/static/user/user_myMessages_back.jpg" mode="widthFix"></image>
 		<!-- 内容 -->
@@ -12,14 +13,18 @@
 			</view>
 			<!-- 内容 -->
 			<view class="myMessages_cont_cen">
+				<!-- 全部留言 -->
 				<view class="myMessages_cont_cenitem" v-show="activetab==1">
-					1
+					<CenterMessage :name="item.name" :time="item.time" :status="item.status" :leave="item.leave" :reply="item.reply" v-for="(item,index) in all.reply" :type="all.type"></CenterMessage>
 				</view>
+				<!-- 已回复 -->
 				<view class="myMessages_cont_cenitem" v-show="activetab==2">
-					2
+					<CenterMessage :name="item.name" :time="item.time" :status="item.status" :leave="item.leave" :reply="item.reply" v-for="(item,index) in replied.reply" :type="replied.type"></CenterMessage>
 				</view>
+				<!-- 未回复-->
 				<view class="myMessages_cont_cenitem" v-show="activetab==3">
-					3
+					<CenterMessage :name="item.name" :time="item.time" :status="item.status" :leave="item.leave" :reply="item.reply" v-for="(item,index) in noreply.reply" :type="noreply.type"></CenterMessage>
+					</view>
 				</view>
 			</view>
 		</view>
@@ -27,10 +32,63 @@
 </template>
 
 <script>
+	import wnavall from "@/components/w-navall/w-navall.vue"
+	import CenterMessage from "@/components/CenterMessage/CenterMessage.vue"
 	export default {
+		components:{
+			wnavall,
+			CenterMessage
+		},
 		data() {
 			return {
-				activetab:1
+				activetab:1,
+				all:{
+					type:true,
+					reply:[
+						{
+							id:1,
+							name:'小飞侠',
+							time:'2021-03-08 16:17:59',
+							status:'已回复',
+							leave:'联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言组件',
+							reply:'联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言'
+						},
+						{
+							id:2,
+							name:'小飞侠1',
+							time:'2021-03-08 16:17:59',
+							status:'未回复',
+							leave:'联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言组件',
+							reply:''
+						}
+					]
+				},
+				replied:{
+					type:false,
+					reply:[
+						{
+							id:1,
+							name:'小飞侠',
+							time:'2021-03-08 16:17:59',
+							status:'已回复',
+							leave:'联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言组件',
+							reply:'联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言'
+						},
+					],
+				},
+				noreply:{
+					type:false,
+					reply:[
+						{
+							id:1,
+							name:'小飞侠1',
+							time:'2021-03-08 16:17:59',
+							status:'未回复',
+							leave:'联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言组件联系常委/我的留言组件',
+							reply:''
+						}
+					]
+				}
 			}
 		},
 		methods: {
@@ -53,4 +111,5 @@
 	.active{color: #ee0000;border-bottom: 2px solid #ee0000;}
 	/* 选项卡内容 */
 	.myMessages_cont_cen{flex: 1;overflow: auto;}
+	.myMessages_cont_cenitem{padding:0px 10px;padding-top: 10px;margin-bottom: 10px;}
 </style>
