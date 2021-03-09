92eff5279aea68d8f3780e5d50add9b92628cfa9
houjingya <1647460898@qq.com>
2021-03-09 11:56:08 +0800

    排行

---
+++ pages/user/userall/sort/sort.vue
@@ -0,0 +1,69 @@
+<template>
+	<view class="item">
+		<view class="itemsort">
+			<u-image src="@/static/user/user_usepa.png" :lazy-load="true" mode="widthFix" width="70rpx" v-if="index==0">
+				<u-loading slot="loading"></u-loading>
+			</u-image>
+			<u-image src="@/static/user/user_usepp.png" :lazy-load="true" mode="widthFix" width="70rpx" v-else-if="index==1">
+				<u-loading slot="loading"></u-loading>
+			</u-image>
+			<u-image src="@/static/user/user_usept.png" :lazy-load="true" mode="widthFix" width="70rpx" v-else-if="index==2">
+				<u-loading slot="loading"></u-loading>
+			</u-image>
+			<view v-else>{{item.sort}}</view>
+		</view>
+		<view class="itemname name" @click="nextpage">
+			<u-image :src="item.image" border-radius="10rpx" :lazy-load="true" mode="widthFix" width="110rpx" v-if="item.image">
+				<u-loading slot="loading"></u-loading>
+			</u-image>
+			<text>{{item.name}}</text>
+		</view>
+		<view class="itemnum">{{item.num}}</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		props:['item','index'],
+		methods:{
+			nextpage(){
+				uni.navigateTo({
+					url:"/pages/text1/liaison/liaison?id="+this.item.id
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.name{
+		flex: 1;
+	}
+	.item{
+		font-size: 36rpx;
+		margin: 20rpx auto;
+		padding: 30rpx 30rpx 0;
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+	}
+	.itemsort{
+		width: 80rpx;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+	}
+	.itemname{
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+		padding: 0 14%;
+		padding-right: 18%;
+	}
+	.itemnum{
+		text-align: right;
+		padding-right: 20rpx;
+		color: orange;
+	}
+</style>
