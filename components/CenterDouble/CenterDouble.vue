e5a8b3fe0ba06af791c0229aa3c50f28f0d9f26b
whx9805 <2224121341@qq.com>
2021-03-10 19:17:04 +0800

    路径

--- components/CenterDouble/CenterDouble.vue
+++ components/CenterDouble/CenterDouble.vue
@@ -1,5 +1,5 @@
 <template>
-	 <navigator url="" hover-class="none"  class="double">
+	 <navigator url="/pages/user/administration/administration" hover-class="none"  class="double">
 		<view class="double_cont">
 			<view class="double_topcont">
 				<text class="double_topleftcont">
